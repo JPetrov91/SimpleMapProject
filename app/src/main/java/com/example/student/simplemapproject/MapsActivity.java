8b6fc50b1c618e15225af599e49b2f5080ca05a2
Jurijs Petrovs <jurijs.petrovs@gmail.com>
2017-02-15 17:51:30 +0200

    commit

--- app/src/main/java/com/example/student/simplemapproject/MapsActivity.java
+++ app/src/main/java/com/example/student/simplemapproject/MapsActivity.java
@@ -1,90 +1,68 @@
 package com.example.student.simplemapproject;
-
-import android.support.v4.app.FragmentActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.widget.AdapterView;
+import android.widget.ImageView;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+public class MapsActivity extends Activity {
+    private Integer THRESHOLD = 2;
+    private DelayAutoCompleteTextView geo_autocomplete;
+    private ImageView geo_autocomplete_clear;
 
-
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
-    private GoogleMap mMap;
-    GPSTracker gps;
-    LatLng locationMarker;
-    Button btnShowLocation;
-
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        btnShowLocation = (Button) findViewById(R.id.button);
 
-        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+        geo_autocomplete_clear = (ImageView) findViewById(R.id.geo_autocomplete_clear);
 
+        geo_autocomplete = (DelayAutoCompleteTextView) findViewById(R.id.geo_autocomplete);
+        geo_autocomplete.setThreshold(THRESHOLD);
+        geo_autocomplete.setAdapter(new GeoAutoCompleteAdapter(this)); // 'this' is Activity instance
+
+        geo_autocomplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View arg0) {
-                // create class object
-                gps = new GPSTracker(MapsActivity.this);
-
-                // check if GPS enabled
-                if(gps.canGetLocation()){
-
-                    double latitude = gps.getLatitude();
-                    double longitude = gps.getLongtitude();
-                    locationMarker = new LatLng(latitude, longitude);
-
-                    // \n is for new line
-                    Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
-                }else{
-                    // can't get location
-                    // GPS or Network is not enabled
-                    // Ask user to enable GPS/network in settings
-                }
-
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                GeoSearchResult result = (GeoSearchResult) adapterView.getItemAtPosition(position);
+                geo_autocomplete.setText(result.getAddress());
             }
         });
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
 
+        geo_autocomplete.addTextChangedListener(new TextWatcher() {
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
 
-        // Add a marker in Sydney and move the camera
-        LatLng defaultLcoc = new LatLng(51.508, -0.076);
-        //LatLng myLoc = new LatLng(gps.getLatitude(), gps.getLongtitude());
-        //System.out.println(myLocation.getLatitude());
-//        if(!(myLoc == null)) {
-//            mMap.addMarker(new MarkerOptions().position(myLoc));
-//            mMap.moveCamera(CameraUpdateFactory.newLatLng(myLoc));
-//        }
-//        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-//        else {
+            }
 
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
 
-            mMap.addMarker(new MarkerOptions().position(locationMarker));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(locationMarker));
-//        }
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.length() > 0)
+                {
+                    geo_autocomplete_clear.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    geo_autocomplete_clear.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        geo_autocomplete_clear.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                geo_autocomplete.setText("");
+            }
+        });
+
     }
 }
